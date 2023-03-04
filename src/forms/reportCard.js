11b67718546d6c15f1e5afcad31ab2f85ca47de5
NashibJr <nashibjr@gmail.com>
2023-03-04 13:21:20 -0800

    Displaying data on the report card

--- src/forms/reportCard.js
+++ src/forms/reportCard.js
@@ -7,6 +7,7 @@ const ReportCard = () => {
   const { id } = useParams();
   const pupils = useSelector((state) => state.pupils.pupils);
   const pupil = pupils.find((pupil) => pupil.id === id);
+  console.log(pupils);
   return (
     <div className="report-content">
       <h2>XXX Primary School Terminal Report</h2>
@@ -36,33 +37,33 @@ const ReportCard = () => {
           <tr>
             <td>MTC</td>
             <td>{pupil.mathematics}</td>
-            <td>1</td>
+            <td>{pupil.agg.mathAgg}</td>
             <td>Excellent</td>
           </tr>
           <tr>
             <td>SCIE</td>
             <td>{pupil.sci}</td>
-            <td>1</td>
+            <td>{pupil.agg.sciAgg}</td>
             <td>Excellent</td>
           </tr>
           <tr>
             <td>SST</td>
             <td>{pupil.sst}</td>
-            <td>1</td>
+            <td>{pupil.agg.sstAgg}</td>
             <td>Excellent</td>
           </tr>
           <tr>
             <td>ENG</td>
             <td>{pupil.english}</td>
-            <td>1</td>
+            <td>{pupil.agg.engAgg}</td>
             <td>Excellent</td>
           </tr>
           <tr>
             <td>
               <strong>Total</strong>
             </td>
-            <td>360</td>
-            <td>4</td>
+            <td>{pupil.total}</td>
+            <td>{pupil.aggregates}</td>
           </tr>
         </table>
       </div>
@@ -80,7 +81,7 @@ const ReportCard = () => {
         </p>
         <p>
           <span>
-            <strong>Grade:</strong> 1
+            <strong>Grade:</strong> {pupil.grade}
           </span>
           <button type="button">Print</button>
         </p>
